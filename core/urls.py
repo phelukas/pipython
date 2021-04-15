4d3e7aa315ba1df35de410c25eb6b5e67e9b00bf
phelukas <ppedrolukas19@gmail.com>
2021-04-14 23:01:02 -0300

    ajuste no response

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from core import views
 
+
 urlpatterns = [
-    path("", views.ListPostAPIView.as_view(), name="post_list"),
+    path("list/post/", views.ListPostAPIView.as_view(), name="post_list"),
     path("create/post/", views.CreatePostAPIView.as_view(), name="post_create"),
     path("update/post/<int:pk>/",
          views.UpdatePostAPIView.as_view(), name="update_post"),
     path("delete/post/<int:pk>/",
          views.DeletePostAPIView.as_view(), name="delete_post"),
-    path("list/posts/<int:pk>/",
+    path("feed/<int:pk>/",
          views.ListOthersPostAPIView.as_view(), name="list_post")
 ]
